df6d98504e87f7adca389200a665899ae64f92ae
SebastianoMV <sebastianovisco@gmail.com>
2022-05-12 15:05:14 +0200

    Prima app Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+   <TitleComp />
+   <HeroComp />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TitleComp from './components/Title.vue'
+import HeroComp from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TitleComp,
+    HeroComp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Jumbo</div>
+</template>
+
+<script>
+export default {
+  name: 'HeroComp'
+}
+</script>
+
+<style lang="scss">
+ 
+</style>
